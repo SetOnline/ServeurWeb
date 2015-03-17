e1b6972b6ee9d9eba508cc4659f7af4b1376f28f
PierreCavalet <Pierre.cavalet@gmail.com>
2015-03-17 16:26:22 +0100

    Ajout du serveur v0.1

---
+++ public/js/jeu.js
@@ -0,0 +1,223 @@
+var carte1 = 0;
+var carte2 = 0;
+var carte3 = 0;
+var nbSetTrouves = 0;
+
+///////////////////
+// COMMUNICATION
+///////////////////
+
+var socket = io(); //.connect('http://localhost:1337/game');
+
+socket.on('timer', function (message) {
+
+    document.getElementById('idTIMER').innerHTML = "il reste " + message + " secondes avant la fin du jeu !";
+
+});
+
+socket.on('Nouvelle partie', function (nouveauJeu) {
+    var infoPartie = JSON.parse(nouveauJeu);
+    var cartesJeu = "";
+    var i;
+    console.log('Nouvelle partie !');
+    for (i = 0; i != 12; ++i) {
+    //    console.log(infoPartie[i].value);
+        cartesJeu += infoPartie[i].value;
+    }
+
+    loadGame(cartesJeu);
+    cardEvents();
+    
+    // suppression sets trouvés
+    var myNode = document.getElementById("trouves");
+    while(myNode.firstChild){
+        myNode.removeChild(myNode.firstChild);
+    }
+});
+
+socket.on('Set valide', function (setQuiEstValide) {
+    console.log("youhou c'est valide");
+    var setPropose = JSON.parse(setQuiEstValide);
+    var setBon = '';
+    setBon += setPropose[0].value;
+    setBon += setPropose[1].value;
+    setBon += setPropose[2].value;
+
+    addSetFound(setBon);
+});
+
+socket.on('Set invalide', function (setQuiEstInvalide) {
+    console.log('pas valide, reessaie p\'tit hacker');
+});
+
+///////////////////
+
+
+function loadGame(combiCartes) {
+    var codeCarte = 0;
+    // 1) suppression
+    var myNode = document.getElementById("jeu");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+
+    // 2) ajout
+    for (var i = 0; i < 12; i++) {
+        codecarte = combiCartes.substring(i*4, (i+1)*4);
+        $("#jeu").append("<img alt=\"carte\" src=\"/img/" + codecarte + ".png\" class=\"carte\" id=\"" + codecarte + "\" />");
+    }
+}
+
+function addSetFound(set) {
+    nbSetTrouves++;
+    var codeCarte = 0;
+    $("#trouves").append("<div class=\"set\" id=\"set" + nbSetTrouves + "\"></div>")
+    for (var i = 0; i < 3; i++) {
+        codecarte = set.substring(i * 4, (i + 1) * 4);
+        console.log("codecarte : " + codecarte);
+        $("#set" + nbSetTrouves).append("<img alt=\"carte\" src=\"/img/" + codecarte + ".png\" class=\"mincarte\" id=\"" + codecarte + "\" />");
+    }
+}
+
+function propriete(prop1, prop2, prop3) {
+    if ((prop1 == prop2) && (prop2 == prop3) && (prop1 == prop3)) {
+        return true;
+    }
+    else if ((prop1 != prop2) && (prop2 != prop3) && (prop1 != prop3)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+function set() {
+    var setCorrect = true;
+    for (var i = 0; i < 4; i++) {
+        var propCorrect = propriete(carte1.charAt(i), carte2.charAt(i), carte3.charAt(i));
+        if (!propCorrect) {
+            setCorrect = false;
+        }
+    }
+    return setCorrect;
+}
+
+function viderCartes() {
+    if (carte1 != 0) {
+        $("#" + carte1).css('filter', 'sepia(0)');
+        $("#" + carte1).css('-webkit-filter', 'sepia(0)');
+        $("#" + carte1).css('-moz-filter', 'sepia(0)');
+        $("#" + carte1).css('-o-filter', 'sepia(0)');
+        $("#" + carte1).css('-ms-filter', 'sepia(0)');
+        carte1 = 0;
+    }
+    if (carte2 != 0) {
+        $("#" + carte2).css('filter', 'sepia(0)');
+        $("#" + carte2).css('-webkit-filter', 'sepia(0)');
+        $("#" + carte2).css('-moz-filter', 'sepia(0)');
+        $("#" + carte2).css('-o-filter', 'sepia(0)');
+        $("#" + carte2).css('-ms-filter', 'sepia(0)');
+        carte2 = 0;
+    }
+    if (carte3 != 0) {
+        $("#" + carte3).css('filter', 'sepia(0)');
+        $("#" + carte3).css('-webkit-filter', 'sepia(0)');
+        $("#" + carte3).css('-moz-filter', 'sepia(0)');
+        $("#" + carte3).css('-o-filter', 'sepia(0)');
+        $("#" + carte3).css('-ms-filter', 'sepia(0)');
+        carte3 = 0;
+    }
+}
+
+
+
+function cardEvents() {
+    $(".carte").click(function () {
+        var selection = false;
+        // codeCarte est le code correspondant au propriété de la carte "rr3s"
+        var codeCarte = $(this).attr('id');
+
+        // on vérifie si la carte est sélectionnée
+        if (carte1 == codeCarte) {
+            carte1 = 0;
+            selection = false;
+            console.log("carte1 == codeCarte");
+        }
+        else if (carte2 == codeCarte) {
+            carte2 = 0;
+            selection = false;
+            console.log("carte2 == codeCarte");
+        }
+        else if (carte3 == codeCarte) {
+            carte3 = 0;
+            selection = false;
+            console.log("carte3 == codeCarte");
+        }
+        else if(carte1 == 0){
+            carte1 = codeCarte;
+            selection = true;
+            console.log("carte1 == 0");
+        }
+        else if(carte2 == 0){
+            carte2 = codeCarte;
+            selection = true;
+            console.log("carte2 == 0");
+        }
+        else if (carte3 == 0) {
+            carte3 = codeCarte;
+            selection = true;
+            console.log("carte3 == 0");
+        }
+        if (selection) {
+            $(this).css('filter', 'sepia(1)');
+            $(this).css('-webkit-filter', 'sepia(1)');
+            $(this).css('-moz-filter', 'sepia(1)');
+            $(this).css('-o-filter', 'sepia(1)');
+            $(this).css('-ms-filter', 'sepia(1)');
+        }
+        else {
+            $(this).css('filter', 'sepia(0)');
+            $(this).css('-webkit-filter', 'sepia(0)');
+            $(this).css('-moz-filter', 'sepia(0)');
+            $(this).css('-o-filter', 'sepia(0)');
+            $(this).css('-ms-filter', 'sepia(0)');    
+        }
+
+        if ((carte1 != 0) && (carte2 != 0) && (carte3 != 0)) {
+            if (set()) {
+                // ici insérer ce qu'on veut mettre quand il trouve un set
+                console.log("Set trouvé correct");
+                console.log("set: " + toStringCards());
+                
+                var tabCartes = [];
+                tabCartes.push({ name: 'carte0', value: carte1 });
+                tabCartes.push({ name: 'carte1', value: carte2 });
+                tabCartes.push({ name: 'carte2', value: carte3 });
+                
+                socket.emit('Set', JSON.stringify(tabCartes));
+
+                viderCartes();
+                
+            }
+            else {
+                // ici insérer ce qu'on veut mettre quand il se trompe
+                console.log("combinaison incorrecte");
+                console.log("set: " + toStringCards());
+                viderCartes();
+            }
+        }
+    });
+}
+
+function toStringCards() {
+    return (carte1 + carte2 + carte3);
+}
+
+
+/*
+$(document).ready(function () {
+    loadGame("123113211111313213213231321323131213213213121321");
+    //addSetFound("123113211111");
+	cardEvents();
+});*/
---
+++ server.js
@@ -0,0 +1,255 @@
+﻿//framework express
+var express = require('express');
+var app = express();
+
+// creation du serveur
+var port = process.env.port || 1337; // port du serveur
+var server = app.listen(port);
+
+// creation comm client serveur
+var io = require('socket.io')(server);
+
+// émetteur d'évènements
+var EventEmitter = require('events').EventEmitter;
+var jeu = new EventEmitter();
+
+// GLOBALES
+var tempsDePartie = 60000; // durée d'une partie (en ms)
+var tempsRestant = 0; // temps restant sur la partie (en s)
+var nbSetsTrouvablesPartieEnCours = 0; // nombre de set partie en cours
+
+
+app.use(express.static(__dirname + "/public"));
+
+// definition de la route de base (accueil)
+app.get('/', function (req, res) {
+    res.setHeader('content-Type', 'text/plain');
+    res.end('Vous êtes à l\'accueil');
+});
+
+// definition de la route du jeu
+app.get('/game', function (req, res) {
+    res.render('game.ejs');
+});
+
+// definition du déclanchement de la fonction timer chaque seconde
+setInterval(function () {
+    tempsRestant--;
+    console.log(tempsRestant);
+    io.sockets.emit('timer', tempsRestant);
+}, 1000);
+
+// fonction utilisé pour générer aléatoirement les 12 cartes distribués
+// @param low : borne -
+// @param high : borne +
+// @return : retourne un entier compris entre low et high
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
+
+
+// abcd
+// avec
+// a : couleur (1 : R, 2 : V, 3 : B)
+// b : remplissage (1 : plein, 2 : rayé, 3 : vide)
+// c : quantité (1, 2, 3)
+// d : forme (1 : éclair, 2 : sphère, 3 : triangle)
+
+
+
+
+
+// genere une combinaison correspondant au code d'une carte
+// @return : retourne une chaine de 4 caractères correspondant au code de la carte généré
+function genererCombinaison() {
+    var combi = '';
+    var i;
+    for (i = 0; i != 4; ++i) {
+        combi += randomInt(1, 4);
+    }
+    return combi;
+}
+
+
+// vérifie si une carte n'a pas été tirée
+// @param tab : tableau contenant les codes des cartes déjà tirées
+// @param carte : le code carte à vérifier
+// @return : vrai si la carte est déjà tirée, faux sinon
+function carteDejaTiree(tab, carte) {
+    var i;
+    for (i = 0; i != tab.length; ++i) {
+        if (tab[i].value == carte) {
+            //console.log('-' + tab[i] + '- == -' + carte + '-');
+            return true;
+        } else {
+            //console.log('-' + tab[i] + '- != -' + carte + '-');
+        }
+    }
+
+    return false;
+}
+
+// génération des 12 cartes et remplissage du fichier JSON
+// @return : un string correspondant au fichier JSON contenant les 12 cartes (un JSON stringifié)
+function genererNouvellePartieEnJSON() {
+    var tabCartes = [];
+    var i;
+    nbSetsTrouvablesPartieEnCours = 0;
+
+    while (nbSetsTrouvablesPartieEnCours == 0) {
+        tabCartes.length = 0; // on delete tout
+        for (i = 0; i != 12; ++i) {
+            var carteTiree = genererCombinaison();
+            if (carteDejaTiree(tabCartes, carteTiree)) {
+                i--;
+            } else {
+                //result.push({ name: name, goals: goals[name] });
+                tabCartes.push({ name: 'carte' + i, value: carteTiree });
+            }
+        }
+        nbSetsTrouvablesPartieEnCours = getNbSolutions(JSON.stringify(tabCartes));
+    }
+
+    tabCartes.push({ name: 'nbSets', value: nbSetsTrouvablesPartieEnCours });
+
+    return JSON.stringify(tabCartes);
+}
+
+
+// détermine le nombre de solution d'un jeu
+// @return : le nombre de solution
+function getNbSolutions(partie) {
+    var tabCartes = JSON.parse(partie);
+    var res = 0;
+
+    for (var i = 0; i != 10; ++i) {
+        for (var j = i + 1; j != 11; ++j) {
+            for (var k = j + 1; k != 12; ++k) {
+                if (estUnSetValide(tabCartes[i].value, tabCartes[j].value, tabCartes[k].value)) {
+                    res++;
+                }
+            }
+        }
+    }
+    return res;
+}
+
+// definition du déclanchement de la nouvelle partie chaque tempsDePartie (variable globale définie en haut du fichier)
+setInterval(function () {
+    var nouveauJeu = genererNouvellePartieEnJSON();
+
+    var infoPartie = JSON.parse(nouveauJeu);
+    console.log('Nouvelle partie ! ' + infoPartie[12].value);
+    tempsRestant = tempsDePartie / 1000;
+    io.sockets.emit('Nouvelle partie', nouveauJeu);
+}, tempsDePartie);
+
+
+// déclancher à chaque connexion d'un client
+// socket est la variable associé à chacun des clients dans la fonction de callback
+// dans cette fonction de callback on défini les fonctions qui vont modifier les variables propres à chaque client
+io.sockets.on('connection', function (socket) {
+
+    console.log('Un client est connecté !');
+    socket.nbSetsValidesRestants = 0;
+
+    // reinitialisation du nombre de set restant
+    jeu.on('Nouvelle partie', function (nouvelleCombi) {
+        socket.nbSetsValidesRestants = 0;
+    });
+
+    // écouteur de l'évennement Set d'un client, vérifie si le set est valide
+    socket.on('Set', function (setJoueur) {
+        var setPropose = JSON.parse(setJoueur);
+        if (estUnSetValide(setPropose[0].value, setPropose[1].value, setPropose[2].value)) {
+            socket.nbSetsValidesRestants -= 1;
+            if (socket.nbSetsValidesRestants < 0)
+                socket.nbSetsValidesRestants = nbSetsTrouvablesPartieEnCours - 1;
+            setPropose.push({ name: 'nbSetsRestants', value: socket.nbSetsValidesRestants });
+            socket.emit('Set valide', JSON.stringify(setPropose));
+        } else {
+            socket.emit('Set invalide', setJoueur);
+        }
+
+    });
+
+    socket.on('Creation compte', function (compteJSON) {
+        var compte = JSON.parse(compteJSON);
+        console.log("creation d'un compte");
+        var mail = compte['adresse_mail'];
+        var pseudo = compte['pseudo'];
+        var mdp = compte['mdp'];
+        console.log("creation du compte : " + mail + " " + pseudo + " " + mdp);
+        var comptepascree = [];
+        comptePasCree.push({ adresse_mail: 'invalide', pseudo: 'true', mdp: 'true' });
+        socket.emit('Compte pas cree', comptePasCree);
+        var compteCree = [];
+        socket.emit('Compte cree', compteCree);
+    });
+
+    socket.on('Connexion', function (compteJSON) {
+        var compte = JSON.parse(compteJSON);
+        console.log("connexion à un compte");
+        var pseudo = compte['pseudo'];
+        var mdp = compte['mdp'];
+        console.log("connexion au compte : " + pseudo + " " + mdp);
+        socket.emit('Resultat connexion', 0);
+    });
+
+    socket.on('Demande mon profil', function () {
+        console.log('Demande mon profil');
+        socket.emit('Reponse mon profil', 0); // ici on ne sait pas encore ce qu'on va envoyer
+    });
+
+    socket.on('Desinscription', function () {
+        var compte = JSON.parse(compteJSON);
+        console.log("Desinscription");
+        var pseudo = compte['pseudo'];
+        var mdp = compte['mdp'];
+        console.log("desinscription du compte : " + pseudo + " " + mdp);
+    });
+
+    socket.on('Modifier profil', function (profilJSON) {
+        var profil = JSON.parse(profilJSON);
+        console.log('Modifier profil');
+        var ancienmdp = profil['ancienmdp'];
+        var nouveaumdp = profil['nouveaumdp'];
+        var nvelavatar = profil['nvelavatar'];
+        console.log("modif profil fini");
+    });
+});
+
+// validation d'une propriete
+// @param prop1 : propriete de la carte 1
+// @param prop2 : propriete de la carte 2
+// @param prop3 : propriete de la carte 3
+// @return : vrai si les 3 valeurs de proprietes sont soit toutes differentes soit toutes identiques, faux sinon
+function propriete(prop1, prop2, prop3) {
+    if ((prop1 == prop2) && (prop2 == prop3) && (prop1 == prop3)) {
+        return true;
+    }
+    else if ((prop1 != prop2) && (prop2 != prop3) && (prop1 != prop3)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+
+}
+
+// validation d'un set
+// @param carte1 : code de la carte 1
+// @param carte2 : code de la carte 2
+// @param carte3 : code de la carte 3
+// @return : vrai si let set est valide, faux sinon
+function estUnSetValide(carte1, carte2, carte3) {
+    var setCorrect = true;
+    for (var i = 0; i < 4; i++) {
+        var propCorrect = propriete(carte1.charAt(i), carte2.charAt(i), carte3.charAt(i));
+        if (!propCorrect) {
+            setCorrect = false;
+        }
+    }
+    return setCorrect;
+}
+
